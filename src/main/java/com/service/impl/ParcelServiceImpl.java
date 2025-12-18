f0e3db65d000041580b8159f6e6cd8eb1bc50bb6
bala-amypo <bala@amypo.in>
2025-12-18 16:08:55 +0000

    User commit

--- src/main/java/com/service/impl/ParcelServiceImpl.java
+++ src/main/java/com/service/impl/ParcelServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Parcel;
 import com.example.demo.repository.ParcelRepository;
 import com.example.demo.service.ParcelService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ParcelServiceImpl implements ParcelService {
 
     private final ParcelRepository parcelRepository;
