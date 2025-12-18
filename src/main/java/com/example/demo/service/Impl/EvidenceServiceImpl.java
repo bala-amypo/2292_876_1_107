2faddee62ab2750d9b94d68c7f813ae63bff2aa2
bala-amypo <bala@amypo.in>
2025-12-18 15:19:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/EvidenceServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.DamageClaim;
+import com.example.demo.model.Evidence;
+import com.example.demo.repository.DamageClaimRepository;
+import com.example.demo.repository.EvidenceRepository;
+import com.example.demo.service.EvidenceService;
+import java.util.List;
+
+public class EvidenceServiceImpl implements EvidenceService {
+
+    private final EvidenceRepository evidenceRepository;
+    private final DamageClaimRepository claimRepository;
+
+    public EvidenceServiceImpl(
+            EvidenceRepository evidenceRepository,
+            DamageClaimRepository claimRepository) {
+
+        this.evidenceRepository = evidenceRepository;
+        this.claimRepository = claimRepository;
+    }
+
+    @Override
+    public Evidence uploadEvidence(Long claimId, Evidence evidence) {
+        DamageClaim claim = claimRepository.findById(claimId)
+                .orElseThrow(() -> new ResourceNotFoundException("Claim not found"));
+
+        evidence.setClaim(claim);
+        return evidenceRepository.save(evidence);
+    }
+
+    @Override
+    public List<Evidence> getEvidenceForClaim(Long claimId) {
+        return evidenceRepository.findByClaim_Id(claimId);
+    }
+}
---
+++ src/main/java/com/example/demo/service/Impl/ParcelServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Parcel;
+import com.example.demo.repository.ParcelRepository;
+import com.example.demo.service.ParcelService;
+
+public class ParcelServiceImpl implements ParcelService {
+
+    private final ParcelRepository parcelRepository;
+
+    public ParcelServiceImpl(ParcelRepository parcelRepository) {
+        this.parcelRepository = parcelRepository;
+    }
+
+    @Override
+    public Parcel addParcel(Parcel parcel) {
+        if (parcelRepository.existsByTrackingNumber(parcel.getTrackingNumber())) {
+            throw new BadRequestException("Tracking number exists");
+        }
+
+        if (parcel.getWeightKg() <= 0) {
+            throw new BadRequestException("Invalid weight");
+        }
+
+        return parcelRepository.save(parcel);
+    }
+
+    @Override
+    public Parcel getByTrackingNumber(String trackingNumber) {
+        return parcelRepository.findByTrackingNumber(trackingNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Parcel not found"));
+    }
+}
