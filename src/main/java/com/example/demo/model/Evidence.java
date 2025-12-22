3da4945c8013a722d49e12709dc9585add023771
bala-amypo <bala@amypo.in>
2025-12-22 01:56:30 +0000

    User commit

--- src/main/java/com/example/demo/model/Evidence.java
+++ src/main/java/com/example/demo/model/Evidence.java
@@ -2,7 +2,7 @@ package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationationType;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
