adc90fe5cd67cf97f650221a012ee008011af393
bala-amypo <bala@amypo.in>
2025-12-22 00:20:29 +0000

    User commit

--- src/main/java/com/service/ParcelService.java
+++ src/main/java/com/service/ParcelService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Parcel;
+import java.util.List;
 
 public interface ParcelService {
 
-    Parcel addParcel(Parcel parcel);
+    Parcel createParcel(Parcel parcel);
+
+    List<Parcel> getAllParcels();
+
+    Parcel getParcel(Long id);
 
     Parcel getByTrackingNumber(String trackingNumber);
 }
