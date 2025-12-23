f730cb73a20afe6079c31528bbcea456e679ed48
bala-amypo <bala@amypo.in>
2025-12-23 08:42:39 +0000

    User commit

--- src/main/java/com/example/demo/model/ClaimRule.java
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 public class ClaimRule {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private double maxAmount;   
