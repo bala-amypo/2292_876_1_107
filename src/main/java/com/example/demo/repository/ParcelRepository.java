6b3ba5ff6a0d17f1fb8f29335a7c09b582ad1492
bala-amypo <bala@amypo.in>
2025-12-25 09:45:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/ParcelRepository.java
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Parcel;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.example.demo.model.Parcel;
 
 public interface ParcelRepository extends JpaRepository<Parcel, Long> {
 
