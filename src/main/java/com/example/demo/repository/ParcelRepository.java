3ff4ac1cc7478fad5ae96234bf0320d06f31773a
bala-amypo <bala@amypo.in>
2025-12-25 05:07:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/ParcelRepository.java
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Parcel;
+
 import java.util.Optional;
 
+/**
+ * IMPORTANT:
+ * - This is NOT a JpaRepository
+ * - Acts as a common parent required by tests
+ */
 public interface ParcelRepository {
 
     boolean existsByTrackingNumber(String trackingNumber);
