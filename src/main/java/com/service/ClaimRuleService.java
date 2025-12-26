bb303738cdeab42d0fa7c96df2ebbbbc8b2d99f1
bala-amypo <bala@amypo.in>
2025-12-26 13:36:26 +0000

    User commit

--- src/main/java/com/service/ClaimRuleService.java
+++ src/main/java/com/service/ClaimRuleService.java
@@ -4,7 +4,8 @@ import com.example.demo.model.ClaimRule;
 import java.util.List;
 
 public interface ClaimRuleService {
-    List<ClaimRule> getAllRules();
 
-    ClaimRule createRule(ClaimRule rule);
+    ClaimRule addRule(ClaimRule rule);
+
+    List<ClaimRule> getAllRules();
 }
